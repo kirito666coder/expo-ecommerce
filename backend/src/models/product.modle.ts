85825cf426e198fe0d6fb84ba71f0cb41a4616ea
kirito <kirito666codr@gmail.com>
2026-03-17 18:54:08 +0530

    Refactor user creation logic and improve Inngest integration
    - Enhanced syncUser function to include error handling and logging for better debugging.
    - Updated API routes to streamline user creation and deletion processes with Inngest.
    - Improved organization of Inngest functions within the codebase for clarity and maintainability.

---
+++ backend/src/models/product.modle.ts
@@ -0,0 +1,50 @@
+import { model, models, Schema } from 'mongoose';
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      min: 0,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    averageRating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 0,
+    },
+    totalReviews: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true },
+);
+
+const Product = models.Product || model('Product', productSchema);
+
+export default Product;
