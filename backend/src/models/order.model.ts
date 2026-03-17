3c31f51e067ca25f79a22382991e274c6109746d
kirito <kirito666codr@gmail.com>
2026-03-17 19:03:01 +0530

    Refactor user model to include phone number and improve validation
    - Added 'phoneNumber' field to user model for enhanced user data.
    - Implemented validation for 'phoneNumber' to ensure proper formatting and required status.
    - Updated user creation logic to accommodate the new phone number field.

---
+++ backend/src/models/order.model.ts
@@ -0,0 +1,98 @@
+import { model, Schema } from 'mongoose';
+
+const orderItemSchema = new Schema({
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+const shippingAddressSchema = new Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  streetAddress: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipCode: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+});
+
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    clerkId: {
+      type: String,
+      required: true,
+    },
+    orderItems: [orderItemSchema],
+    shippingAddress: {
+      type: shippingAddressSchema,
+      required: true,
+    },
+    paymentResult: {
+      id: String,
+      status: String,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'shipped', 'delivered'],
+      default: 'pending',
+    },
+    deliveredAt: {
+      type: Date,
+    },
+    shippedAt: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+const orderModel = model('Order', orderSchema);
+
+export default orderModel;
