976be6e47943801dc0c30c60c8fd4dbbf5596cd5
kirito <kirito666codr@gmail.com>
2026-03-27 10:15:11 +0530

    Update package dependencies and configuration for mobile app
    - Added new dependencies: babel-preset-expo, nativewind, and prettier-plugin-tailwindcss to enhance styling and formatting capabilities.
    - Updated package-lock.json to reflect the latest versions of tailwindcss and other related packages.
    - Modified app.json to specify the bundler as metro for improved build configuration.
    - Adjusted ESLint configuration import path for consistency.
    - Enhanced TypeScript configuration to include additional files for better type management.
    - Updated layout and index components to utilize Tailwind CSS for styling improvements.

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex h-full items-center justify-center">
+      <Text className="text-3xl font-bold text-red-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
