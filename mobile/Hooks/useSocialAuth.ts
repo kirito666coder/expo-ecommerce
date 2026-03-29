19357b68bc1bbd8cb3dd628450c84018c7568a09
kirito <kirito666codr@gmail.com>
2026-03-29 14:47:48 +0530

    Add social authentication and restructure mobile app layout
    - Introduced `useSocialAuth` hook for handling Google OAuth authentication.
    - Created `AuthRoutesLayout` to manage authentication routes and redirection based on user sign-in status.
    - Developed `AuthScreen` for user login with Google, including loading state management.
    - Established tab navigation layout with `TabsLayout` for authenticated users.
    - Added placeholder screens for Shop, Cart, and Profile tabs.
    - Included new image assets and updated TypeScript definitions for better type management.

---
+++ mobile/Hooks/useSocialAuth.ts
@@ -0,0 +1,25 @@
+import { useSSO } from '@clerk/clerk-expo';
+import { useState } from 'react';
+import { Alert } from 'react-native';
+
+export function useSocialAuth() {
+  const [loadingStrategy, setLoadingStrategy] = useState<boolean>(false);
+  const { startSSOFlow } = useSSO();
+
+  const handleSocialAuth = async (strategy: 'oauth_google') => {
+    setLoadingStrategy(true);
+
+    try {
+      const { createdSessionId, setActive } = await startSSOFlow({ strategy });
+      if (createdSessionId && setActive) {
+        await setActive({ session: createdSessionId });
+      }
+    } catch {
+      Alert.alert(`Error Failed to sign in with ${strategy}. Please try again.`);
+    } finally {
+      setLoadingStrategy(false);
+    }
+  };
+
+  return { loadingStrategy, handleSocialAuth };
+}
---
+++ mobile/app/(auth)/index.tsx
@@ -0,0 +1,44 @@
+import { View, Text, Image, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { useSocialAuth } from '@/Hooks/useSocialAuth';
+import { icons } from '@/constants/icons';
+
+export default function AuthScreen() {
+  const { loadingStrategy, handleSocialAuth } = useSocialAuth();
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Image source={icons.auth_img} className="h-[60%] w-screen" resizeMode="contain" />
+
+      <View className="mt-10 w-[80%] gap-2">
+        <TouchableOpacity
+          className="flex-row items-center justify-center rounded-full border border-gray-300 bg-white px-6 py-1"
+          onPress={() => handleSocialAuth('oauth_google')}
+          disabled={loadingStrategy}
+          style={{
+            shadowOffset: { width: 0, height: 1 },
+            shadowOpacity: 0.1,
+            elevation: 2,
+          }}
+        >
+          {loadingStrategy ? (
+            <ActivityIndicator />
+          ) : (
+            <View className="flex-row items-center justify-center">
+              <Image source={icons.google} className="mr-3 size-10" resizeMode="contain" />
+              <Text className="text-base font-medium text-black">Continue with Google</Text>
+            </View>
+          )}
+        </TouchableOpacity>
+      </View>
+
+      <Text className="mt-6 px-2 text-center text-sm leading-4 text-gray-500">
+        By signin up, you agree to our <Text className="text-blue-500">Terms</Text>
+        {', '}
+        <Text className="text-blue-500">Privacy policy</Text>
+        {', '}
+        <Text className="text-blue-500">Cookie use</Text>
+      </Text>
+    </View>
+  );
+}
